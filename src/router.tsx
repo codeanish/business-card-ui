93e8a02f5b317891b8e1710a15eb93fa776738d1
codeanish <anish@codeanish.com>
2023-09-28 06:50:52 -0700

    Creaed homepage

--- src/router.tsx
+++ src/router.tsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Card from "./pages/Card";
 import NotFoundPage from "./pages/404";
 import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
 
 const Router = () => {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<NotFoundPage/>} />
+            <Route path="/" element={<Home/>} />
             <Route path="/signin" element={<SignIn/>} />
-            <Route path="/:username" element={<Card/>} />
+            <Route path="/u/:username" element={<Card/>} />
             <Route path="/404" element={<NotFoundPage />} />
           	<Route path="*" element={<Navigate to="/404" />} />
         </Routes>
